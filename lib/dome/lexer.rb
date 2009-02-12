cbe792166f076116efe65f238112825911ab6637
karottenreibe <karottenreibe@gmail.com>
2009-02-12 16:50:53 +0100

    lexer now in own file

---
+++ lib/dome/lexer.rb
@@ -0,0 +1,123 @@
+# This is Dome, a pure Ruby HTML DOM parser with very simple XPath support.
+#
+# If you want to find out more or need a tutorial, go to
+# http://dome.rubyforge.org/
+# You'll find a nice wiki there!
+#
+# Author::      Fabian Streitel (karottenreibe)
+# Copyright::   Copyright (c) 2008 Fabian Streitel
+# License::     Boost Software License 1.0
+#               For further information regarding this license, you can go to
+#               http://www.boost.org/LICENSE_1_0.txt
+#               or read the file LICENSE distributed with this software.
+# Homepage::    http://dome.rubyforge.org/
+# Git repo::    http://rubyforge.org/scm/?group_id=7589
+#
+# Contains the Lexer that transforms the input String into a list
+# of tokens for the Parser.
+#
+
+module Dome
+
+    class Token
+
+        ##
+        # The type of the token - Symbol
+        # Possible values:
+        # - :left_bracket
+        # - :right_bracket
+        # - :equal
+        # - :whitespace
+        # - :text
+        # - :element_end
+        # - :cdata_start
+        # - :cdata_end
+        attr_accessor :type
+
+        ##
+        # The value of the token - String or +nil+
+        attr_accessor :value
+
+        ##
+        # Initializes the Token's +type+ and +value+ fields.
+        #
+        def initialize type, value = nil
+            @type, @value = type, value
+        end
+
+    end
+
+    ##
+    # Splits a given String into small components that are consumed by the Parser.
+    #
+    class Lexer
+
+        ##
+        # Initializes the Lexer with the input +string+.
+        #
+        def initialize string
+            @string, @pos, @tokens = string, 0, []
+            self.split!
+        end
+
+        ##
+        # Retrieves the next token from the input.
+        #
+        def next
+            @tokens[@pos]
+        end
+
+        ##
+        # Advances by one token.
+        #
+        def next!
+            @pos += 1
+        end
+
+        ##
+        # Whether or not the lexer has more tokens in it's storage.
+        #
+        def next?
+            @pos < @tokens.length
+        end
+
+        ##
+        # Returns an object that can be used to backtrack to the current position by calling
+        # +undo+.
+        #
+        def trace
+            @pos
+        end
+
+        ##
+        # Backtraces to the position identified by the +trace+ object.
+        #
+        def undo trace
+            @pos = trace
+        end
+
+        protected
+
+        ##
+        # Splits the input up into Tokens and stores them in +@tokens+.
+        #
+        def split!
+            @string.split(/<|=|\s|\/>|>|<!\[CDATA\[|\]\]>/).each do |token|
+                type = 
+                    case token
+                    when '<' then :left_bracket
+                    when '>' then :right_bracket
+                    when '=' then :equal
+                    when /\s/ then :whitespace
+                    when '/>' then :element_end
+                    when '<![CDATA[' then :cdata_start
+                    when ']]>' then :cdata_end
+                    else :text
+                    end
+                @tokens << Token.new type, token
+            end
+        end
+
+    end
+end
+
