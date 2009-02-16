14b8b28da8ff0a190c89acaa5a0fef113b14823c
karottenreibe <karottenreibe@gmail.com>
2009-02-16 20:15:28 +0100

    forgot to add dom test file test was wrong by the way

---
+++ test/dom_tests.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+# This is Dome, a pure Ruby HTML DOM parser with very simple XPath support.
+#
+# If you want to find out more or need a tutorial, go to
+# http://dome.rubyforge.org/
+# You'll find a nice wiki there!
+#
+# Author::      Fabian Streitel (karottenreibe)
+# Copyright::   Copyright (c) 2008 Fabian Streitel
+# License::     Boost Software License 1.0
+#               For further information regarding this license, you can go to
+#               http://www.boost.org/LICENSE_1_0.txt
+# Homepage::    http://dome.rubyforge.org/
+# Git repo::    http://rubyforge.org/scm/?group_id=7589
+#
+
+require 'test/unit'
+require 'lib/dome'
+
+class ParserTests < Test::Unit::TestCase
+    include Dome
+
+    def testEmptyElem
+        tree = Dome "<doctor />"
+        assert_kind_of Tree, tree
+        assert_equal false, tree.root.children.empty?
+        assert_kind_of Element, tree.root.children[0]
+        assert_equal "doctor", tree.root.children[0].tag
+    end
+
+    def testData
+        tree = Dome "<donna>noble</donna>"
+        assert_kind_of Tree, tree
+
+        assert_equal false, tree.root.children.empty?
+        donna = tree.root.children[0]
+        assert_kind_of Element, donna
+        assert_equal "donna", donna.tag
+
+        assert_equal false, donna.children.empty?
+        noble = donna.children[0]
+        assert_kind_of Data, noble
+        assert_equal "noble", noble.value
+        assert_equal false, noble.cdata
+    end
+
+end
+
